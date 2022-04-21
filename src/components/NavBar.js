3ca6697919141a1e7ad27baf6c8dd9e965194d33
AsyrieChaeranie <asyrie.ac@gmail.com>
2022-04-22 01:39:14 +0700

    Progress

---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Navbar = () => {
+  const classes = useStyles();
+
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          className={classes.menuButton}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h5" className={classes.title}>
+        Musicophile 
+        </Typography>
+        <Button color="inherit" >
+          log-out 
+        </Button>
+      </Toolbar>
+     {/* <ModalDialog open={open} handleClose={handleClose} /> */}
+    </AppBar>
+  );
+};
+
+export default Navbar;
